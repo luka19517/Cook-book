c66913b0a8e4f36d16624d5c69b0139a6c42917a
luka19517 <luka.djorovic3010@gmail.com>
2022-11-27 18:53:44 +0100

    vue project cleanup  + added primevue dependency

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
 import router from './router/router';
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+
+app.use(router);
+app.use(PrimeVue);
+
+
+app.component()
+
+app.mount('#app')
